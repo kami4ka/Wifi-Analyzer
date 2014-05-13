f24b9e55120c793c4370fae5bc3f7803199969a6
Oleg Kulyk <okami4kak@gmail.com>
2014-05-13 12:04:30 +0300

    Added signal class for signal colors

--- src/com/kami/wificharts/MainActivity.java
+++ src/com/kami/wificharts/MainActivity.java
@@ -25,7 +25,6 @@ import android.widget.ListView;
 
 public class MainActivity extends Activity {
     
-    private static final int HISTORY_SIZE = 10;
     private XYPlot strengthPlot;
     private XYPlot timePlot;
     
@@ -33,6 +32,17 @@ public class MainActivity extends Activity {
     WifiScanReceiver wifiReciever;
     ListView list;
     String wifis[];
+    
+    static String[] ColourValues = new String[] { 
+        "FF0000", "00FF00", "0000FF", "FFFF00", "FF00FF", "00FFFF", "000000", 
+        "800000", "008000", "000080", "808000", "800080", "008080", "808080", 
+        "C00000", "00C000", "0000C0", "C0C000", "C000C0", "00C0C0", "C0C0C0", 
+        "400000", "004000", "000040", "404000", "400040", "004040", "404040", 
+        "200000", "002000", "000020", "202000", "200020", "002020", "202020", 
+        "600000", "006000", "000060", "606000", "600060", "006060", "606060", 
+        "A00000", "00A000", "0000A0", "A0A000", "A000A0", "00A0A0", "A0A0A0", 
+        "E00000", "00E000", "0000E0", "E0E000", "E000E0", "00E0E0", "E0E0E0", 
+    };
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- src/com/kami/wificharts/Signal.java
+++ src/com/kami/wificharts/Signal.java
@@ -1,5 +1,48 @@
 package com.kami.wificharts;
 
+import com.androidplot.xy.SimpleXYSeries;
+
 public class Signal {
+    
+    private static final int HISTORY_SIZE = 10;
+    private String mName;
+    private String mColor;
+    private SimpleXYSeries mSeries = null;
+    
+    public Signal(String name, String color) {
+        mSeries = new SimpleXYSeries(mName);
+        mSeries.useImplicitXVals();
+        setName(name);
+        setColor(color);
+    }
+    
+    public void addToSeries(int level) {
+        if (mSeries.size() > HISTORY_SIZE) {
+            mSeries.removeFirst();
+        }
+        
+        mSeries.addLast(null, level);
+    }
+    
+    public SimpleXYSeries getSeries() {
+        return mSeries;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public void setName(String mName) {
+        this.mName = mName;
+    }
+
+    public String getColor() {
+        return mColor;
+    }
+
+    public void setColor(String mColor) {
+        this.mColor = mColor;
+    }
+    
 
 }
